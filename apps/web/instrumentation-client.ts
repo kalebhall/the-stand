1c875db8a6b26ec55927c8a908d8a61b132a5554
kalebhall <kaleb@kalebhall.com>
2026-03-12 13:50:20 -0700

    Update instrumentation-client.ts

--- apps/web/instrumentation-client.ts
+++ apps/web/instrumentation-client.ts
@@ -1,33 +1,3 @@
-'use client';
-
-import { getSentryClientDsn, isSentryEnabled, loadSentrySdk } from '@/src/lib/sentry';
-
-const initializeSentryClient = async (): Promise<void> => {
-  if (!isSentryEnabled() || !getSentryClientDsn()) {
-    return;
-  }
-
-  const sdk = await loadSentrySdk();
-
-  if (!sdk?.init) {
-    return;
-  }
-
-  sdk.init({
-    dsn: getSentryClientDsn(),
-    enabled: true,
-    tracesSampleRate: 1,
-    replaysSessionSampleRate: 0.1,
-    replaysOnErrorSampleRate: 1
-  });
-};
-
-void initializeSentryClient();
-
-// This file configures the initialization of Sentry on the client.
-// The added config here will be used whenever a users loads a page in their browser.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
